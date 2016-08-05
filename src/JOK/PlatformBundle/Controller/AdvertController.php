0cda4e8c70013c59f9590916eed795490d05022e
jokerhahaha <liondejude@hotmail.com>
2016-08-05 16:03:43 +0200

    controller CRUD

--- src/JOK/PlatformBundle/Controller/AdvertController.php
+++ src/JOK/PlatformBundle/Controller/AdvertController.php
@@ -6,49 +6,73 @@ namespace JOK\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdvertController extends Controller
 {
-    public function indexAction()
-    {
-        // On veut avoir l'URL de l'annonce d'id 5.
-        $url = $this->get('router')->generate(
-            'jok_platform_view', // 1er argument : le nom de la route
-            array('id' => 5)    // 2e argument : les valeurs des paramètres
-            );
-        // $url vaut « /platform/advert/5 »
-        
-        return new Response("L'URL de l'annonce d'id 5 est : ".$url);
+  public function indexAction($page)
+  {
+    // On ne sait pas combien de pages il y a
+    // Mais on sait qu'une page doit être supérieure ou égale à 1
+    if ($page < 1) {
+      // On déclenche une exception NotFoundHttpException, cela va afficher
+      // une page d'erreur 404 (qu'on pourra personnaliser plus tard d'ailleurs)
+      throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
     }
 
-    public function viewAction($id, Request $request)
-    {
-        $tag = $request->query->get('tag');
+    // Ici, on récupérera la liste des annonces, puis on la passera au template
 
-        return new Response("Affichage de l'annonce d'id : ".$id." avec le tag : ".$tag);
+    // Mais pour l'instant, on ne fait qu'appeler le template
+    return $this->render('JOKPlatformBundle:Advert:index.html.twig');
+  }
+
+  public function viewAction($id)
+  {
+    // Ici, on récupérera l'annonce correspondante à l'id $id
+
+    return $this->render('JOKPlatformBundle:Advert:view.html.twig', array(
+      'id' => $id
+    ));
+  }
+
+  public function addAction(Request $request)
+  {
+    // La gestion d'un formulaire est particulière, mais l'idée est la suivante :
+
+    // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
+    if ($request->isMethod('POST')) {
+      // Ici, on s'occupera de la création et de la gestion du formulaire
+
+      $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+      // Puis on redirige vers la page de visualisation de cettte annonce
+      return $this->redirectToRoute('jok_platform_view', array('id' => 5));
     }
 
-    public function viewSlugAction($fileName, $year, $_format)
-    {
-        return new Response(
-            "On pourrait afficher l'annonce correspondant au
-            fileName '".$fileName."', créée en ".$year." et au format ".$_format."."
-            );
+    // Si on n'est pas en POST, alors on affiche le formulaire
+    return $this->render('JOKPlatformBundle:Advert:add.html.twig');
+  }
+
+  public function editAction($id, Request $request)
+  {
+    // Ici, on récupérera l'annonce correspondante à $id
+
+    // Même mécanisme que pour l'ajout
+    if ($request->isMethod('POST')) {
+      $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
+
+      return $this->redirectToRoute('jok_platform_view', array('id' => 5));
     }
 
-    public function addAction()
-    {
+    return $this->render('JOKPlatformBundle:Advert:edit.html.twig');
+  }
 
-    }
+  public function deleteAction($id)
+  {
+    // Ici, on récupérera l'annonce correspondant à $id
 
-    public function editAction()
-    {
-        
-    }
+    // Ici, on gérera la suppression de l'annonce en question
 
-    public function deleteAction()
-    {
-        
-    }
+    return $this->render('JOKPlatformBundle:Advert:delete.html.twig');
+  }
 }
